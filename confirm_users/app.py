b5b2ab926e30c054cba941a2cb5306e84d6286ce
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-08-05 11:57:00 +0100

    Fix django and pyforms apps configuration

--- confirm_users/app.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class ConfirmUsersConfig(AppConfig):
-    name = 'confirm_users'
-    verbose_name = 'Confirm users'
-
-    def ready(self):
-        import confirm_users.signals
--- confirm_users/apps/__init__.py
+++ confirm_users/apps/__init__.py
@@ -1,1 +1,14 @@
-from .dashboard import Dashboard
+from django.apps import AppConfig
+
+
+class ConfirmUsersConfig(AppConfig):
+    name = "confirm_users"
+    verbose_name = "Confirm users"
+
+    def ready(self):
+        from .. import signals
+
+        from .dashboard import Dashboard
+
+        global Dashboard
+
