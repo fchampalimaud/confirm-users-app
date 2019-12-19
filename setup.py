99b75eae47ea89054413540af088604ff2a75eaa
Hugo Cachitas <hugo.cachitas@research.fchampalimaud.org>
2019-12-19 23:00:35 +0000

    bump version to 0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="confirm_users",
-    version="0.0",
+    version="0.1",
     description="confirm auto registered users app",
     long_description=README,
     long_description_content_type="text/markdown",
